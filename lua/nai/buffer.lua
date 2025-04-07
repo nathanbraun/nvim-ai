4f8b5b9a8bbb1fade78ec433713f19c08a96159c
Nathan Braun <nathanericbraun@gmail.com>
2025-04-07 14:25:51 -0500

    feat: set keymaps

--- lua/nai/buffer.lua
+++ lua/nai/buffer.lua
@@ -94,10 +94,13 @@ function M.activate_buffer(bufnr)
   end, { desc = 'Add a new user message' })
 
   -- Add key mappings (buffer-local)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>r', ':NAIChat<CR>',
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>ac', ':NAIChat<CR>',
     { noremap = true, silent = true, desc = 'Continue chat' })
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>m', ':NAINewMessage<CR>',
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>au', ':NAINewMessage<CR>',
+    { noremap = true, silent = true, desc = 'Add new user message' })
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>am', ':NAIModel<CR>',
     { noremap = true, silent = true, desc = 'Add new user message' })
 
   -- Explicitly apply syntax highlighting
